d2cd191ce93fddd3b6a57cbceed974874ecbae34
Kravchenko21 <Yana_sin82@mail.ru>
2024-08-06 16:20:09 +0500

    Исправлено вызов функции

--- Module_3_1.1.py
+++ Module_3_1.1.py
@@ -1,35 +1,34 @@
 calls = 0
 
+
 def count_calls():
     global calls
-    calls = calls + 1
-
-print(calls)
+    calls = calls +1
+    return calls
+    print(count_calls())
+    
 count_calls()
 
 
 def string_info(*args):
-    def count_calls():
-          global calls      
-          calls = calls + 1 
+    global calls
+    calls = calls + 1
     print(args)
-    print(calls)
+    print(count_calls())
+
 string_info('Capybara'.upper(), 'Capybara'.lower(), len('Capybara'))
 string_info('Armagedon'.upper(), 'Armagedon'.lower(), len('Armagedon'))
 count_calls()
 
-
-
 def is_contains(string, list_to_search):
-    def count_calls():
-        global calls
-        calls = calls + 1
     if string in list_to_search:
         print(True)
     else:
         print(False)
+        global calls
+        calls = calls + 1
     print(string, list_to_search)
-    print(calls)
+    print(count_calls()) 
 
 is_contains('Urban', ['ban', 'BaNaN', 'Urban'])
 is_contains('cycle', ['recycling', 'cyclic'])
