80ab29d3e4c20a17288ea2395cb02174d9a75506
Kravchenko21 <Yana_sin82@mail.ru>
2024-08-08 15:09:31 +0500

    Вызов подсчета количества функций -верно

--- Module_3_1.1.py
+++ Module_3_1.1.py
@@ -5,20 +5,20 @@ def count_calls():
     global calls
     calls = calls +1
     return calls
-    print(count_calls())
+
     
-count_calls()
+    count_calls()
 
 
 def string_info(*args):
     global calls
     calls = calls + 1
     print(args)
-    print(count_calls())
-
+    # print(count_calls())
+    count_calls()
 string_info('Capybara'.upper(), 'Capybara'.lower(), len('Capybara'))
 string_info('Armagedon'.upper(), 'Armagedon'.lower(), len('Armagedon'))
-count_calls()
+
 
 def is_contains(string, list_to_search):
     if string in list_to_search:
@@ -28,8 +28,7 @@ def is_contains(string, list_to_search):
         global calls
         calls = calls + 1
     print(string, list_to_search)
-    print(count_calls()) 
+    count_calls()
 
 is_contains('Urban', ['ban', 'BaNaN', 'Urban'])
 is_contains('cycle', ['recycling', 'cyclic'])
-count_calls()
